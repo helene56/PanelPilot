f4e275c4e192b82e2c22bfd5c7dadefd275f3f69
helene <helene.maja@hotmail.dk>
2025-03-27 22:01:30 +0100

    decided to track lib contents

---
+++ lib/ui/images.c
@@ -0,0 +1,7 @@
+#include "images.h"
+
+const ext_img_desc_t images[3] = {
+    { "water-icon", &img_water_icon },
+    { "plant-icon", &img_plant_icon },
+    { "plant-icon2", &img_plant_icon2 },
+};
---
+++ lib/ui/screens.c
@@ -0,0 +1,122 @@
+#include <string.h>
+
+#include "screens.h"
+#include "images.h"
+#include "fonts.h"
+#include "actions.h"
+#include "vars.h"
+#include "styles.h"
+#include "ui.h"
+
+#include <string.h>
+
+objects_t objects;
+lv_obj_t *tick_value_change_obj;
+
+void create_screen_main() {
+    lv_obj_t *obj = lv_obj_create(0);
+    objects.main = obj;
+    lv_obj_set_pos(obj, 0, 0);
+    lv_obj_set_size(obj, 320, 240);
+    lv_obj_set_style_bg_color(obj, lv_color_hex(0xff9cdad1), LV_PART_MAIN | LV_STATE_DEFAULT);
+    {
+        lv_obj_t *parent_obj = obj;
+        {
+            lv_obj_t *obj = lv_label_create(parent_obj);
+            lv_obj_set_pos(obj, 104, 47);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_label_set_text(obj, "Hello, world!");
+        }
+        {
+            // water
+            lv_obj_t *obj = lv_btn_create(parent_obj);
+            objects.water = obj;
+            lv_obj_set_pos(obj, 19, 79);
+            lv_obj_set_size(obj, 50, 50);
+            lv_obj_set_style_bg_image_tiled(obj, false, LV_PART_MAIN | LV_STATE_DEFAULT);
+            lv_obj_set_style_bg_color(obj, lv_color_hex(0xff70b6ed), LV_PART_MAIN | LV_STATE_DEFAULT);
+            {
+                lv_obj_t *parent_obj = obj;
+                {
+                    lv_obj_t *obj = lv_label_create(parent_obj);
+                    lv_obj_set_pos(obj, 0, 0);
+                    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+                    lv_label_set_text(obj, "");
+                    lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
+                }
+            }
+        }
+        {
+            // plant
+            lv_obj_t *obj = lv_btn_create(parent_obj);
+            objects.plant = obj;
+            lv_obj_set_pos(obj, 99, 79);
+            lv_obj_set_size(obj, 50, 50);
+            lv_obj_set_style_bg_color(obj, lv_color_hex(0xff5be0b6), LV_PART_MAIN | LV_STATE_DEFAULT);
+            lv_obj_set_style_bg_image_tiled(obj, false, LV_PART_MAIN | LV_STATE_DEFAULT);
+            {
+                lv_obj_t *parent_obj = obj;
+                {
+                    lv_obj_t *obj = lv_label_create(parent_obj);
+                    lv_obj_set_pos(obj, 0, 0);
+                    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+                    lv_label_set_text(obj, "");
+                    lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
+                }
+            }
+        }
+        {
+            lv_obj_t *obj = lv_imagebutton_create(parent_obj);
+            lv_obj_set_pos(obj, 232, 47);
+            lv_obj_set_size(obj, 0, 64);
+            lv_obj_set_style_bg_image_src(obj, &img_undefined, LV_PART_MAIN | LV_STATE_DEFAULT);
+        }
+        {
+            lv_obj_t *obj = lv_img_create(parent_obj);
+            lv_obj_set_pos(obj, 99, 79);
+            lv_obj_set_size(obj, 50, 50);
+            lv_img_set_src(obj, &img_plant_icon2);
+            lv_img_set_zoom(obj, 20);
+        }
+        {
+            lv_obj_t *obj = lv_img_create(parent_obj);
+            lv_obj_set_pos(obj, 19, 79);
+            lv_obj_set_size(obj, 50, 50);
+            lv_img_set_src(obj, &img_water_icon);
+            lv_img_set_zoom(obj, 20);
+        }
+        {
+            lv_obj_t *obj = lv_img_create(parent_obj);
+            lv_obj_set_pos(obj, 232, 160);
+            lv_obj_set_size(obj, 50, 50);
+            lv_img_set_src(obj, &img_plant_icon);
+            lv_img_set_zoom(obj, 20);
+        }
+    }
+    
+    tick_screen_main();
+}
+
+void tick_screen_main() {
+}
+
+
+
+typedef void (*tick_screen_func_t)();
+tick_screen_func_t tick_screen_funcs[] = {
+    tick_screen_main,
+};
+void tick_screen(int screen_index) {
+    tick_screen_funcs[screen_index]();
+}
+void tick_screen_by_id(enum ScreensEnum screenId) {
+    tick_screen_funcs[screenId - 1]();
+}
+
+void create_screens() {
+    lv_disp_t *dispp = lv_disp_get_default();
+    lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), false, LV_FONT_DEFAULT);
+    lv_disp_set_theme(dispp, theme);
+    
+    create_screen_main();
+}
