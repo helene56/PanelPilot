dcd75a1bba0fc030ed81ad1047878ff5b7ee5a8c
helene <helene.maja@hotmail.dk>
2025-04-02 20:57:28 +0200

    remove unused pin definitions for touch screen

--- include/pins.h
+++ include/pins.h
@@ -7,12 +7,10 @@
 #define SPI_PORT spi0
 constexpr uint PIN_MISO {16};
 constexpr uint PIN_CS   {17};
-// constexpr uint TOUCH_CS {20}; // not needed here
 constexpr uint PIN_SCK  {18};
 constexpr uint PIN_MOSI {19};
 constexpr uint PIN_D_C  {22}; // data selection, command: 0, display data: 1
 constexpr uint PIN_RST  {21};
-// constexpr uint TOUCH_PEN  {26}; // not needed here
 
 
 #endif // PINS
